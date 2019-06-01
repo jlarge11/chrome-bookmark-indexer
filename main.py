a66699a35c99b06883883b8b5040e527419e5a4d
Justin Large <justinlarge1974@gmail.com>
2019-06-01 14:27:43 -0700

    Now turning each bookmark into a shell script.

--- main.py
+++ main.py
@@ -1,31 +1,24 @@
 import json
 from pathlib import Path
-
-INDENT = 4
-
-def determine_indent(level):
-    return ' ' * level * INDENT
-
-def print_folder(name, level):
-    indent = determine_indent(level)
-    print(f'{indent}- {name}')
-
-def print_bookmark(b, level):
-    indent = determine_indent(level)
-    name = b['name']
-    url = b['url']
-    print(f'{indent}    {name} ({url})')
+import os
+import shutil
 
 def process_folder(folder, parent_path, parent_level_number):
     folder_name = folder['name']
     path = f"{parent_path}/{folder_name}"
     level_number = parent_level_number + 1
 
-    print_folder(folder_name, level_number)
+    os.makedirs(path)
 
     for v in folder['children']:
         if v['type'] == 'url':
-            print_bookmark(v, level_number)
+            name = v['name']
+            url = v['url']
+
+            with open(f'{path}/{name}.sh', 'w') as f:
+                f.write('#!/bin/bash\n')
+                f.write(f'google-chrome {url} &')
+                
         else:
             process_folder(v, path, level_number)
 
@@ -33,6 +26,10 @@ user_home = str(Path.home())
 with open(f'{user_home}/.config/google-chrome/Default/Bookmarks') as f:
     bookmarks = json.load(f)
 
+bookmark_folder = f'{user_home}/bookmarks'
+shutil.rmtree(bookmark_folder, ignore_errors = True)
+os.makedirs(bookmark_folder)
+
 for k, v in bookmarks['roots'].items():
     if type(v) is dict:
-        process_folder(v, '.', 0)
+        process_folder(v, bookmark_folder, 0)
